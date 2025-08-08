e2c14c1a622d5b3f52d622b4780780bdd523edfe
jeff-zucker <dubzed@gmail.com>
2025-08-07 19:39:35 -0700

    chore(viewer): remove console log

--- local2pod.js
+++ local2pod.js
@@ -8,7 +8,6 @@ async function local2pod(){
   catch(e){ console.log(e); }
   for(let file of files){
     const from = `file:///`+process.cwd()+ '/' + file;
-console.log (from); process.exit();
     const to   = `https://solidproject.solidcommunity.net/catalog/${file}`;
     try {
       const getResponse = await client.fetch(from);
